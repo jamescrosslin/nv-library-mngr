e2296a25e4d95628b409dda197ad8ae5ca516ba6
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-07 11:44:15 -0700

    added tests to ensure db connection and server start

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { Book } = require("../models");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  const books = await Book.findAll();
+  res.json(books);
 });
 
 module.exports = router;
