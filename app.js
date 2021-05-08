8c7ab25d89be7273a7e041adac478fb83ea88476
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-07 17:13:41 -0700

    fixed typo in routing

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", booksRouter);
+app.use("/books", booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
