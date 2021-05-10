39f2929be12a964a7b7d4553ad8a1de0fcca9221
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-10 11:38:16 -0700

    enforced style guide

--- models/book.js
+++ models/book.js
@@ -1,5 +1,5 @@
-"use strict";
-const { Model } = require("sequelize");
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Book extends Model {
     /**
@@ -7,9 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
+    // static associate(models) {
+    // define association here
+    // }
   }
   Book.init(
     {
@@ -36,8 +36,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "Book",
-    }
+      modelName: 'Book',
+    },
   );
   return Book;
 };
