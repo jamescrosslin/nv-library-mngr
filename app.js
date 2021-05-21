d42f7c00ae520809fd4e3ad6aeb585c48e253de9
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-20 19:41:44 -0700

    cleaned up dependencies

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -34,7 +33,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
