e7b82b97dd9245f47d1288e6ea0171316c04cb2a
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-21 14:36:58 -0700

    clarified code comment

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 $(document).ready(() => {
-  // jQuery function for loading DataTable
+  // jQuery function for loading DataTable dependency from CDN
   $('#books').DataTable({
     // removes default table search
     searching: false,
